9e777f731011a3e48af702bbef151c55e159cdf6
modeverv <modeverv@gmail.com>
2018-11-01 00:23:31 +0900

    Initial Commit

---
+++ NorainuQuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NorainuQuizApp
+//
+//  Created by norainu on 2018/11/01.
+//  Copyright © 2018 norainu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
